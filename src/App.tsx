aa4b3692a20114c0ef5f59e7f2c92459d331bef1
Alexey Astafev <alexey.astafev@sirenltd.com>
2022-06-29 21:26:07 +0300

    a bit corrected

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
-const App = () => (
-    <div>
-    </div>
-)
+const App = () => {
+    return (
+        <div>
+            Content here like header, slider, pages, etc
+        </div>
+    );
+};
 
 export default App;
