d8096c75e448291700b49e1c5255e21c8c89af7e
Alexey Astafev <alexey.astafev@sirenltd.com>
2022-07-05 23:01:55 +0300

    almost added redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {setupStore} from "./store/store";
+import {Provider} from "react-redux";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 
+const store = setupStore()
+
 root.render(
     <React.StrictMode>
-        <App/>
+        <Provider store={store}>
+            <App/>
+        </Provider>
     </React.StrictMode>
 );
