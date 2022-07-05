e1d30723fff665be71e3bb2ed5fb848e6909f6d2
Alexey Astafev <alexey.astafev@sirenltd.com>
2022-07-05 21:30:04 +0300

    corrected and typing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {FC} from 'react';
 import Header from "./components/Header";
 
-const App = () => (
+const App: FC = () => (
     <div>
         <Header/>
     </div>
