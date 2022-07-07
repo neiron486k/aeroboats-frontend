482e83773062a5187cf06e3143d93045baf62a81
foxma <Fox1901@bk.ru>
2022-07-07 19:30:50 +0300

    added catalog

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React, {FC} from 'react';
 import Header from "./components/Header";
 import {CssBaseline} from "@mui/material";
+import Catalog from "./sections/Catalog";
 
 const App: FC = () => (
     <div>
         <CssBaseline/>
         <Header/>
+        <Catalog/>
     </div>
 )
 
