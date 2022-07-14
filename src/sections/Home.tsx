bb5ac2b29c9ecfbed1b424ed9262936291debe7a
Alexey Astafev <alexey.astafev@sirenltd.com>
2022-07-14 16:52:22 +0300

    chenged images for example

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -5,13 +5,13 @@ import Slide from "../components/Carousel/models/Slide";
 
 const slides: Slide[] = [
     {
-        image: 'https://cont.ws/uploads/pic/2020/11/59_putorana.jpg',
+        image: 'https://st18.stpulscen.ru/images/product/325/898/871_big.jpg',
         leftContent: 'Have a nice boats with us',
         centerContent: 'We are the best developers of boats',
         rightContent: 'We hava a lot of experience in building boats'
     },
     {
-        image: 'https://cont.ws/uploads/pic/2020/11/59_putorana.jpg',
+        image: 'https://1687772.ssl.1c-bitrix-cdn.ru/upload/iblock/b25/b2583ef8a81270d9d54520532221e0e7.jpg?162497169160190',
         leftContent: 'Have a nice fishing',
         centerContent: 'Buy asap awesome a boat!',
         rightContent: 'Have a good evening'
