ceedaed94831cd057e4af5ea2b057ecb18dad9ae
Alex Astafev <efsneiron@gmail.com>
2022-11-24 23:23:42 +0300

    added avatar slider to product page

--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -1,23 +1,30 @@
-import { Grid } from '@mui/material';
+import { Box, Grid, Typography } from '@mui/material';
 import React, { FC } from 'react';
 import { useParams } from 'react-router-dom';
+import Slider from 'react-slick';
 
+import Title from '../../components/Title';
 import { useGetProductQuery } from '../../services/product';
-import Navigator from './Navigator';
+import ProductAvatar from './ProductAvatar';
 
 const Product: FC = () => {
   const { id } = useParams();
   const { data: product, isSuccess } = useGetProductQuery(Number(id));
 
   return (
-    <Grid container sx={{ background: 'white', height: '100vh' }}>
-      <Grid item md={6}>
-        {isSuccess && <Navigator images={product.images} />}
-      </Grid>
-      <Grid item md={6}>
-        content
-      </Grid>
-    </Grid>
+    <Box sx={{ background: 'white', height: '100vh' }}>
+      {isSuccess && (
+        <Grid container>
+          <Grid item xs={12} md={6}>
+            <ProductAvatar height="50vh" images={product.images} />
+          </Grid>
+          <Grid item xs={12} md={6} height="50vh" p={2}>
+            <Title sx={{ color: '#000' }}>{product.name}</Title>
+            <Typography>{product.description}</Typography>
+          </Grid>
+        </Grid>
+      )}
+    </Box>
   );
 };
 
