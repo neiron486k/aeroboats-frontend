cff0d334da7e35589f04ca072e65b65717ce77f7
Alex Astafev <efsneiron@gmail.com>
2022-11-28 00:00:01 +0300

    fixed pesponsible of mobile product page

--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, Typography } from '@mui/material';
+import { Box, Grid, Typography, useMediaQuery, useTheme } from '@mui/material';
 import React, { FC } from 'react';
 import { useParams } from 'react-router-dom';
 
@@ -8,15 +8,17 @@ import ProductAvatar from './ProductAvatar';
 const Product: FC = () => {
   const { id } = useParams();
   const { data: product, isSuccess } = useGetProductQuery(Number(id));
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down('md'));
 
   return (
-    <Box sx={{ background: 'white', height: '100vh' }}>
+    <Box sx={{ background: 'white' }} height={matches ? '100%' : '100vh'}>
       {isSuccess && (
         <Grid container>
           <Grid item xs={12} md={6}>
             <ProductAvatar height="50vh" images={product.images} />
           </Grid>
-          <Grid item xs={12} md={6} height="50vh" p={2}>
+          <Grid item xs={12} md={6} p={2} height={matches ? '100%' : '50vh'}>
             <Typography variant="h4" align="center" gutterBottom>
               {product.name}
             </Typography>
