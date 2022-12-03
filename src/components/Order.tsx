26dfaaa94da04c1fc04cc6dcd815cbf5711893ed
Alex Astafev <efsneiron@gmail.com>
2022-12-03 23:32:08 +0300

    corrected order select

--- src/components/Order.tsx
+++ src/components/Order.tsx
@@ -9,7 +9,7 @@ import {
   MenuItem,
   TextField,
 } from '@mui/material';
-import Select from '@mui/material/Select';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 import React, { FC, useState } from 'react';
 
 import { useGetProductsQuery } from '../services/product';
@@ -17,9 +17,15 @@ import { useGetProductsQuery } from '../services/product';
 const Order: FC = () => {
   const [open, setOpen] = useState<boolean>(false);
   const { data: products, isSuccess } = useGetProductsQuery(10);
+  const [selectValue, setSelectValue] = useState('');
 
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+
+  const handleSelectChange = (event: SelectChangeEvent) => {
+    const value = event.target.value.toString();
+    setSelectValue(value);
+  };
 
   return (
     <>
@@ -35,14 +41,27 @@ const Order: FC = () => {
           </FormControl>
           <FormControl fullWidth>
             <InputLabel id="product-select-label">Товар *</InputLabel>
-            <Select fullWidth variant="outlined" labelId="product-select-label" id="product-select" label="Товар *">
-              {isSuccess && products.results.map((product) => <MenuItem value={product.id}>{product.name}</MenuItem>)}
+            <Select
+              fullWidth
+              variant="outlined"
+              labelId="product-select-label"
+              id="product-select"
+              label="Товар *"
+              value={selectValue}
+              onChange={handleSelectChange}
+            >
+              {isSuccess &&
+                products.results.map((product) => (
+                  <MenuItem key={product.id} value={product.id}>
+                    {product.name}
+                  </MenuItem>
+                ))}
             </Select>
           </FormControl>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Отмена</Button>
-          <Button onClick={handleClose}>Заказать</Button>
+          <Button type="submit">Заказать</Button>
         </DialogActions>
       </Dialog>
     </>
