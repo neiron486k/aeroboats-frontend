b7eaab3d31814ff84813823db431a982350ec885
Alex Astafev <efsneiron@gmail.com>
2022-12-09 10:05:08 +0300

    corrected the order form

--- src/components/Order/CreateOrderForm.tsx
+++ src/components/Order/CreateOrderForm.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, FormControl, MenuItem, TextField } from '@mui/material';
+import { Box, Button, FormControl, InputLabel, MenuItem, TextField } from '@mui/material';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import React, { ChangeEvent, FC, FormEvent, useCallback, useState } from 'react';
 import { GoogleReCaptcha, GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
@@ -40,6 +40,7 @@ const CreateOrderForm: FC<CreateOrderFormProps> = ({ products, onSubmit, onClose
   const handleClose = () => {
     onClose();
     setValues(InitValuesState);
+    setRefreshReCaptcha((r) => !r);
   };
 
   const handleChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement> | SelectChangeEvent) => {
@@ -62,32 +63,46 @@ const CreateOrderForm: FC<CreateOrderFormProps> = ({ products, onSubmit, onClose
   );
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} noValidate>
       <FormControl fullWidth sx={{ '& .MuiTextField-root': { mb: 2 } }}>
         <TextField
           name="full_name"
+          label="Ф.И.О"
+          required
           value={values.full_name}
           onChange={handleChange}
           error={errors.full_name.length > 0}
           helperText={errors.full_name}
+          sx={{ mt: 1 }}
         />
         <TextField
           name="phone"
+          label="Телефон"
+          required
           value={values.phone}
           onChange={handleChange}
           error={errors.phone.length > 0}
           helperText={errors.phone}
         />
-        <Select name="product" value={values.product} onChange={handleChange} error={errors.product.length > 0}>
+        <GoogleReCaptchaProvider reCaptchaKey={process.env.REACT_APP_RECAPTCHA_KEY}>
+          <GoogleReCaptcha onVerify={onVerify} action="order" refreshReCaptcha={refreshReCaptcha} />
+        </GoogleReCaptchaProvider>
+      </FormControl>
+      <FormControl fullWidth>
+        <InputLabel id="product-select-label">Товар *</InputLabel>
+        <Select
+          name="product"
+          label="Товар *"
+          value={values.product}
+          onChange={handleChange}
+          error={errors.product.length > 0}
+        >
           {products.map((item) => (
             <MenuItem key={item.id} value={item.id}>
               {item.name}
             </MenuItem>
           ))}
         </Select>
-        <GoogleReCaptchaProvider reCaptchaKey={process.env.REACT_APP_RECAPTCHA_KEY}>
-          <GoogleReCaptcha onVerify={onVerify} action="order" refreshReCaptcha={refreshReCaptcha} />
-        </GoogleReCaptchaProvider>
       </FormControl>
       <Box sx={{ display: 'flex', justifyContent: 'end', mt: 2 }}>
         <Button type="submit" variant="contained" color="secondary" sx={{ mr: 1 }}>
